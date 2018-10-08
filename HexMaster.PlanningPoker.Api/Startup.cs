a51799b3c47dbf7a23d45743be2bcdd77b9f74b6
Eduard <ekeilholz@outlook.com>
2018-10-08 11:15:19 +0200

    Refactored all to planning poker

--- HexMaster.PlanningPoker.Api/Startup.cs
+++ HexMaster.PlanningPoker.Api/Startup.cs
@@ -29,7 +29,7 @@ namespace HexMaster.PlanningPoker.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async Task Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
