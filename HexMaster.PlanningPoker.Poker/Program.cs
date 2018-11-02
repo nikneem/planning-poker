97df1ba84084349a615c45ec9e87b2a61805b2ab
Eduard Keilholz <e.keilholz@4dotnet.nl>
2018-11-02 20:07:51 +0100

    Whoop!

--- HexMaster.PlanningPoker.Poker/Program.cs
+++ HexMaster.PlanningPoker.Poker/Program.cs
@@ -14,11 +14,19 @@ namespace HexMaster.PlanningPoker.Poker
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    IHostingEnvironment env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", false, true)
+                        .AddJsonFile("secrets/poker.secret.json", true);
+                })
                 .UseStartup<Startup>();
     }
 }
--- HexMaster.PlanningPoker.Poker/Startup.cs
+++ HexMaster.PlanningPoker.Poker/Startup.cs
@@ -72,6 +72,7 @@ namespace HexMaster.PlanningPoker.Poker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
