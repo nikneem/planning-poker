76539289832dbd60141fc75d1d942fb928b8d39b
Eduard Keilholz <e.keilholz@4dotnet.nl>
2018-11-02 19:14:01 +0100

    Adding deployments and allow local config per deploymeny

--- HexMaster.PlanningPoker.Spa/src/environments/environment.prod.ts
+++ HexMaster.PlanningPoker.Spa/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   google_client_id:
     '993647663827-8nsrvt062jm7ras9mrgnd1krr48v69m9.apps.googleusercontent.com',
   google_secret: 'oEjga_kXPRthjToQpls5n_Bj',
-  backendApi: 'https://localhost:55500',
-  callbackUrl: 'https://localhost:55000/callback'
+  backendApi: 'http://api.planning-poker.net',
+  liveApi: 'http://live.planning-poker.net'
 };
